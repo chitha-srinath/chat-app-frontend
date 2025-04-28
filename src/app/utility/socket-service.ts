a9a8faa0af9cc988ea4d693cf10c54951bb4da82
chitha-srinath <92856666+chitha-srinath@users.noreply.github.com>
2025-04-28 16:01:37 +0530

    latest socket-service.ts

--- src/app/utility/socket-service.ts
+++ src/app/utility/socket-service.ts
@@ -1,62 +1,76 @@
-// // src/app/services/socket.service.ts
+// src/app/services/socket.service.ts
 
-// import { Injectable } from '@angular/core';
-// import { io, Socket } from 'socket.io-client';
-// import { Observable } from 'rxjs';
-// import { ChatEvents } from '../types/socket-events';
+import { Injectable } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+import { Observable } from 'rxjs';
 
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class SocketService {
-//   private socket: Socket;
+export enum ChatEvents {
+  SEND_MESSAGE = 'send_message',
+  UPDATE_MESSAGE = 'update_message',
+  DELETE_MESSAGE = 'delete_message',
+  REACT_MESSAGE = 'react_message',
+  REPLY_MESSAGE = 'reply_message',
 
-//   constructor() {
-//     this.socket = io(`${environment.socketUrl}`, {
-//         transports: ['websocket'],
-//         autoConnect: false,
-//         reconnectionAttempts: 5,
-//         reconnectionDelay: 1000,
-//         reconnectionDelayMax: 5000,
-//         timeout: 20000,
-//         pingInterval: 25000,
-//         pingTimeout: 60000,
-//         upgrade: false,
-//         forceNew: true,
-//         multiplex: false,
-//         withCredentials: true,
+  USER_ONLINE = 'user_online',
+  USER_OFFLINE = 'user_offline',
+  USER_TYPING = 'user_typing',
 
-//     });
-
-//     this.socket.on('connect', () => {
-//       console.log('Connected to socket server:', this.socket.id);
-//     });
-
-//     this.socket.on('disconnect', () => {
-//       console.log('Disconnected from socket server');
-//     });
-//   }
+  UPDATE_UNREAD_COUNT = 'update_unread_count',
+}
 
 
+@Injectable({
+  providedIn: 'root',
+})
+export class SocketService {
+  private socket: Socket;
 
-//   // Emit events
-//   emit(event: ChatEvents, data: any) {
-//     this.socket.emit(event, data);
-//   }
+  constructor() {
+    this.socket = io(`${environment.socketUrl}`, {
+        transports: ['websocket'],
+        autoConnect: false,
+        reconnectionAttempts: 5,
+        reconnectionDelay: 1000,
+        reconnectionDelayMax: 5000,
+        timeout: 20000,
+        pingInterval: 25000,
+        pingTimeout: 60000,
+        upgrade: false,
+        forceNew: true,
+        multiplex: false,
+        withCredentials: true,
 
-//   // Listen to events
-//   listen<T>(event: ChatEvents): Observable<T> {
-//     return new Observable((subscriber) => {
-//       this.socket.on(event, (data: T) => {
-//         subscriber.next(data);
-//       });
-//     });
-//   }
+    });
 
-//   // Disconnect manually
-//   disconnect() {
-//     if (this.socket) {
-//       this.socket.disconnect();
-//     }
-//   }
-// }
+    this.socket.on('connect', () => {
+      console.log('Connected to socket server:', this.socket.id);
+    });
+
+    this.socket.on('disconnect', () => {
+      console.log('Disconnected from socket server');
+    });
+  }
+
+
+
+  // Emit events
+  emit(event: ChatEvents, data: any) {
+    this.socket.emit(event, data);
+  }
+
+  // Listen to events
+  listen<T>(event: ChatEvents): Observable<T> {
+    return new Observable((subscriber) => {
+      this.socket.on(event, (data: T) => {
+        subscriber.next(data);
+      });
+    });
+  }
+
+  // Disconnect manually
+  disconnect() {
+    if (this.socket) {
+      this.socket.disconnect();
+    }
+  }
+}
